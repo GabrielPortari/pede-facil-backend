72d552ea3aba4d718405fd589a77b3df42fc70f5
Gabriel <contato.gabrielportari@gmail.com>
2026-02-21 20:01:12 -0300

    style: format imports in app.module.ts for better readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
 import { AuthModule } from './auth/auth.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), FirebaseModule.forRoot(), AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    FirebaseModule.forRoot(),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
